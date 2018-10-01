1a449f6cf65143d21138d1cf0d26ed0d7710078c
Jaeseo Park <jaeseo_park@intuit.com>
2018-10-01 00:34:33 -0600

    orchestrate the download at the top level

--- nico_crawler.py
+++ nico_crawler.py
@@ -1,11 +1,17 @@
-import json
-import requests
+import os
+import logging
 
 from MyList import MyList
 from Video import Video
 
+log = logging.getLogger(__name__)
+
 if __name__ == '__main__':
     url = 'http://www.nicovideo.jp/mylist/48382005'
+
+    if not os.path.exists('config.json'):
+        raise FileNotFoundError(
+            'config not found. Create a json file with the following details: download_location, email, password')
 
     videos = []
     if 'mylist' in url:
@@ -13,3 +19,8 @@ if __name__ == '__main__':
         videos = ml.videos
     else:
         videos = [Video(url)]
+
+    for video in videos:
+        log.info('Start {}'.format(video))
+        video.download()
+        log.info('Done  {}'.format(video))
