98d4191fb88119e5a7a1f0de6e94c8d712f5d760
Jaeseo Park <jaeseo_park@intuit.com>
2018-10-05 00:57:37 -0600

    bringing back title

--- core/video.py
+++ core/video.py
@@ -9,12 +9,15 @@ log = logging.getLogger(__name__)
 
 
 class Video:
-    def __init__(self, video_id=None, url=None):
-        self.video_id = None
+    def __init__(self, video_id=None, url=None, title=None):
+        if (1 if video_id else 0) + (1 if url else 0) != 1:
+            raise AssertionError('Need one of video_id and url')
+
         if url:
             self.video_id = url.split('/')[-1]
         if video_id:
             self.video_id = video_id
+        self.title = title
 
     def __str__(self):
         return '{} {}'.format(self.title, self.video_id) if self.title else self.video_id
