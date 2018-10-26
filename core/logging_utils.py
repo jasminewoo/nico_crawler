ca974c61a16b5a947cf741fa8830ffbf53c6ecbf
Jaeseo Park <jaeseo_park@intuit.com>
2018-10-25 20:15:18 -0600

    log formatting

--- core/logging_utils.py
+++ core/logging_utils.py
@@ -14,7 +14,7 @@ def get_file_log_handler(name='default'):
     if not os.path.exists(k_LOGS_FOLDER):
         os.mkdir(k_LOGS_FOLDER)
     handler = logging.FileHandler('{}/{}.log'.format(k_LOGS_FOLDER, name))
-    fh_fmt = logging.Formatter("%(asctime)s %(levelname)s %(threadName)s %(funcName)20s %(message)s")
+    fh_fmt = logging.Formatter("%(asctime)s %(levelname)8s %(threadName)9s %(funcName)s %(message)s")
     fh_fmt.datefmt = '%Y-%m-%d %H:%M:%S'
     handler.setFormatter(fh_fmt)
     handler.setLevel(logging.DEBUG)
