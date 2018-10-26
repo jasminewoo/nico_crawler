b71a7682d70167a57e445d5efa8d99e10bbcb1ab
Jaeseo Park <jaeseo_park@intuit.com>
2018-10-26 12:36:06 -0600

    added test case

--- tests/custom_youtube_dl_test.py
+++ tests/custom_youtube_dl_test.py
@@ -8,7 +8,8 @@ class CustomYoutubeDLTest(unittest.TestCase):
     def test_filename_sanitize(self):
         kvps = {
             'メトロノーム 歌ってみた / pazi': 'メトロノーム 歌ってみた pazi',
-            '【覚えて歌お！】カラオケで歌えるボーカロイド曲集09_4月号B': '[覚えて歌お!]カラオケで歌えるボーカロイド曲集09_4月号B'
+            '【覚えて歌お！】カラオケで歌えるボーカロイド曲集09_4月号B': '[覚えて歌お!]カラオケで歌えるボーカロイド曲集09_4月号B',
+            '【2人で】ン（Ver）【初心者Fとmorlbon】':'[2人で]ン(Ver)[初心者Fとmorlbon]'
         }
         for org_title, expected in kvps.items():
             actual = custom_youtube_dl.sanitize_title(org_title)
