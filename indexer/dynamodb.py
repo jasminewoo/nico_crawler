3cde6de37297441b2baddeb92160c3d9cb818d78
Jaeseo Park <jaeseo_park@intuit.com>
2018-10-25 22:15:09 -0600

    everything is retried now

--- indexer/dynamodb.py
+++ indexer/dynamodb.py
@@ -2,8 +2,9 @@ import logging
 import time
 from datetime import datetime, tzinfo, timedelta
 
-import boto3
+from boto3 import Session
 from boto3.dynamodb.conditions import Key
+from botocore.exceptions import ClientError
 
 from indexer.indexer_service import Indexer
 
@@ -11,6 +12,9 @@ logging.getLogger('boto').setLevel('CRITICAL')
 logging.getLogger('boto3').setLevel('CRITICAL')
 logging.getLogger('botocore').setLevel('CRITICAL')
 log = logging.getLogger(__name__)
+
+RETRY_EXCEPTIONS = ('ProvisionedThroughputExceededException',
+                    'ThrottlingException')
 
 k_LAST_EVALUATED_KEY = 'LastEvaluatedKey'
 k_VIDEO_ID = 'video_id'
@@ -21,7 +25,7 @@ k_LAST_MODIFIED_UTC = 'last_modified_utc'
 class DynamoDbIndexer(Indexer):
     def __init__(self, config=None):
         Indexer.__init__(self, config=config)
-        session = boto3.Session(
+        session = Session(
             aws_access_key_id=config['aws_access_key_id'],
             aws_secret_access_key=config['aws_secret_access_key'],
             region_name=config['aws_region']
@@ -34,10 +38,8 @@ class DynamoDbIndexer(Indexer):
         return list(map(lambda x: x[k_VIDEO_ID], items))
 
     def _get_items_by_status(self, status, max_result_set_size=None):
-        response = self.table.query(
-            IndexName='video_status-index',
-            KeyConditionExpression=Key(k_VIDEO_STATUS).eq(status)
-        )
+        response = run_with_retry(self.table.query, IndexName='video_status-index',
+                                  KeyConditionExpression=Key(k_VIDEO_STATUS).eq(status))
         len_limit = min(response['Count'], max_result_set_size if max_result_set_size else 1000000)
         return response['Items'][:len_limit]
 
@@ -49,55 +51,53 @@ class DynamoDbIndexer(Indexer):
         self._update_by_video_id(video_id=video_id, key=k_VIDEO_STATUS, value=status)
 
     def _get_item(self, video_id):
-        response = self.table.get_item(
-            Key={
-                k_VIDEO_ID: video_id
-            }
-        )
+        response = run_with_retry(self.table.get_item, Key={k_VIDEO_ID: video_id})
         return response['Item'] if 'Item' in response else None
 
     def _update_by_video_id(self, video_id, key, value):
-        self.table.update_item(
-            Key={
-                k_VIDEO_ID: video_id
-            },
-            UpdateExpression="set {}=:m, {}=:v".format(k_LAST_MODIFIED_UTC, key),
-            ExpressionAttributeValues={
-                ':v': value,
-                ':m': utcnow_in_string()
-            },
-            ReturnValues="UPDATED_NEW"
-        )
+        run_with_retry(self.table.update_item,
+                       Key={k_VIDEO_ID: video_id},
+                       UpdateExpression="set {}=:m, {}=:v".format(k_LAST_MODIFIED_UTC, key),
+                       ExpressionAttributeValues={
+                           ':v': value,
+                           ':m': utcnow_in_string()
+                       },
+                       ReturnValues="UPDATED_NEW"
+                       )
 
     def get_all_video_ids_as_set(self):
+        log.info('Getting all video ids from DynamoDB...')
         to_return = set()
         metadata = {k_LAST_EVALUATED_KEY: None}
         while True:
-            try:
-                if metadata[k_LAST_EVALUATED_KEY]:
-                    response = self.table.scan(ExclusiveStartKey=metadata[k_LAST_EVALUATED_KEY])
-                else:
-                    response = self.table.scan()
+            if metadata[k_LAST_EVALUATED_KEY]:
+                response = run_with_retry(self.table.scan, ExclusiveStartKey=metadata[k_LAST_EVALUATED_KEY])
+            else:
+                response = run_with_retry(self.table.scan)
 
-                to_return |= set(map(lambda x: x[k_VIDEO_ID], response['Items']))
-                log.info('len(set)={}'.format(len(to_return)))
+            to_return |= set(map(lambda x: x[k_VIDEO_ID], response['Items']))
+            log.info('len(set)={}'.format(len(to_return)))
 
-                if response['Count'] == 0 or k_LAST_EVALUATED_KEY not in response:
-                    log.info('All items downloaded')
-                    break
+            if response['Count'] == 0 or k_LAST_EVALUATED_KEY not in response:
+                log.info('All items downloaded')
+                break
 
-                metadata[k_LAST_EVALUATED_KEY] = response[k_LAST_EVALUATED_KEY]
+            metadata[k_LAST_EVALUATED_KEY] = response[k_LAST_EVALUATED_KEY]
 
-                time.sleep(10)
-            except Exception as e:
-                if 'ProvisionedThroughputExceededException' in str(e):
-                    # This is a weird way of handling exception, but I can't seem to reference botocore.errorfactory.ProvisionedThroughputExceededException
-                    log.info('DynamoDB Read Capacity exceeded... retrying in 30s')
-                    time.sleep(30)
-                else:
-                    raise
+            time.sleep(10)
 
         return to_return
+
+
+def run_with_retry(func, **kwarg):
+    while True:
+        try:
+            return func(**kwarg)
+        except ClientError as e:
+            if e.response['Error']['Code'] not in RETRY_EXCEPTIONS:
+                raise
+            log.info('DynamoDB Provisioned Capacity exceeded... retrying in 30s')
+            time.sleep(30)
 
 
 class SimpleUTC(tzinfo):
