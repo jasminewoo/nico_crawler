c2cd21f2485ce162046672d87151e67f370ac96a
Jaeseo Park <jaeseo_park@intuit.com>
2018-10-26 12:27:22 -0600

    made global_config accessible from anywhere

--- core/global_config.py
+++ core/global_config.py
@@ -5,9 +5,15 @@ import os
 class Config(dict):
     def __init__(self, config_file_paths):
         dict.__init__(self)
+        prefix = ''
+        cwd = os.getcwd().split('/')
+        while cwd[-1] != 'nico_crawler':
+            cwd.remove(cwd[-1])
+            prefix += '../'
         for config_file_path in config_file_paths:
-            if os.path.exists(config_file_path):
-                with open(config_file_path, 'r') as fp:
+            path = prefix + config_file_path
+            if os.path.exists(path):
+                with open(path, 'r') as fp:
                     kvp = json.load(fp)
                     for key in kvp:
                         self[key] = kvp[key]
