7be3aa8b9730820ea083e510b3be47ac98027748
Jaeseo Park <jaeseo_park@intuit.com>
2018-10-28 19:19:58 -0600

    unittest for multi_replace and bugfix

--- core/string_utils.py
+++ core/string_utils.py
@@ -12,6 +12,6 @@ def multi_replace(input_string, replace_dict):
 
     rep = replace_dict
 
-    rep = dict((re.escape(k), v) for k, v in rep.iteritems())
+    rep = dict((re.escape(k), v) for k, v in rep.items())
     pattern = re.compile("|".join(rep.keys()))
     return pattern.sub(lambda m: rep[re.escape(m.group(0))], input_string)
---
+++ tests/string_utils_test.py
@@ -0,0 +1,11 @@
+import unittest
+
+from core import string_utils
+
+
+class StringUtilsTest(unittest.TestCase):
+    def test_multi_replace(self):
+        org_str = 'replace me'
+        replace_definition = {'replace': 'result', 'me': 'success'}
+        new_str = string_utils.multi_replace(org_str, replace_definition)
+        self.assertEqual('result success', new_str)
